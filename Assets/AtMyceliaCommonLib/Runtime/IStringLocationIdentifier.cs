892995c031bb1f5086996302bcdb17dd4ddbce01
CG-Tespy <gbcabreracgt@gmail.com>
2026-05-22 10:57:33 -0400

    Fixed typo

--- Assets/AtMyceliaCommonLib/Runtime/IStringLocationIdentifier.cs
+++ Assets/AtMyceliaCommonLib/Runtime/IStringLocationIdentifier.cs
@@ -6,6 +6,6 @@ namespace AtMycelia
     /// </summary>
     public interface IStringLocationIdentifier
     {
-        string LocationIdentifier { get; };
+        string LocationIdentifier { get; }
     }
 }
